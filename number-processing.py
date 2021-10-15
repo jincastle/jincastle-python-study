5b51bbfab676d55161cf49b25db6ea6efca646da
jinsungPark <dksk0101@gmail.com>
2021-10-15 17:09:48 +0900

    1week

---
+++ number-processing.py
@@ -0,0 +1,19 @@
+print(abs(-5)) # 절대값 5출력
+
+print(pow(4,2)) # 4의 2 제곱근 값 16 출력
+
+print(max(5,12)) #최대값 12 출력
+
+print(max(5,12)) #최소값 5 출력
+
+print(round(3.14)) # 반올림 3
+
+
+#Math
+from math import *
+print(floor(3.74)) # 내림 3
+
+print(ceil(3.14)) # 올림 4
+
+print(sqrt(16)) # 제곱근 4*4 4
+
---
+++ random.py
@@ -0,0 +1,21 @@
+from random import *;
+from math import *;
+x = random.random()
+print(x) # 랜덤으로 값을 뽑음
+
+print(int(random())) # 정수값만 랜덤으로 뽑음
+
+print(int(random() * 10)) # 10 미만의 값을 랜덤으로 뽑음
+
+print(int(random() * 10)+1) #1 ~ 10 미만의 값을 랜덤으로 뽑음
+
+
+# range(1,46) 1부터 46 미만의 값을 가진다.
+# range(1,7) = [1, 2, 3, 4, 5, 6]
+print(randrange(1,46)) #1 ~ 46 미만의 값 랜덤으로 뽑음
+
+a = range(1,7)
+print(a)
+random.shuffle(a) # 시퀀스를 섞음 하지만 인덱스 고유값은 그대로 유지
+random.choice(a) # 랜덤으로 하나의 값 선택
+
