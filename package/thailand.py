31bf066a68a861f75d8a83039660c75fed842804
jinsungPark <dksk0101@gmail.com>
2021-10-23 18:36:38 +0900

    3week_clear

--- package/thailand.py
+++ package/thailand.py
@@ -1,3 +1,10 @@
 class ThailandPackage:
     def detail(self):
         print("[태국 패키지 3박 5일] 방콕, 파타야 여행 (야시장 투어) 50만원")
+
+if __name__ == "__main__":
+    print("모듈을 직접 실행")
+    trip_to = ThailandPackage()
+    trip_to.detail()
+else:
+    print("외부에서 모듈을 직접 호출")
--- package_play.py
+++ package_play.py
@@ -1,7 +1,16 @@
-# import package.thailand
+# import package.thailand #패키지
 # trip_to= package.thailand.ThailandPackage()
 # trip_to.detail()
 
-from package.thailand import ThailandPackage
-trip_to = ThailandPackage()
-trip_to.detail()
+# from package.thailand import ThailandPackage #패키지
+# trip_to = ThailandPackage()
+# trip_to.detail()
+
+# from package import * #패키지
+# trip_to= thailand.ThailandPackage()
+# trip_to.detail()
+
+import inspect
+import random
+from package import *
+print(inspect.getfile(japan))
